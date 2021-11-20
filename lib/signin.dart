06b3e28b3e608027cfe333cf38831a600bc74796
Andrey Paskal <apaskal@gmail.com>
2021-11-20 20:04:55 +0300

    Code formatting

--- lib/signin.dart
+++ lib/signin.dart
@@ -15,61 +15,71 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     final Color bgColor = Color(int.parse("0xFF007291"));
     return Scaffold(
-        appBar: AppBar(title: const Text('https://intranet.signapi.art sign in'),backgroundColor: bgColor,),
-        body: SingleChildScrollView(child: 
-          Padding(
-            padding: EdgeInsets.all(40),
-        child: Column(
-          children: <Widget>[
-          SizedBox(
-            width: 120,
-            height: 120,
-            child: Image.asset('asset/images/stamp.png'),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top:20,bottom:10),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'User Name',
-                hintText: 'Enter valid mail id as abc@gmail.com'),
+      appBar: AppBar(
+        title: const Text('https://intranet.signapi.art sign in'),
+        backgroundColor: bgColor,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                width: 120,
+                height: 120,
+                child: Image.asset('asset/images/stamp.png'),
               ),
-            ),
-          Padding(
-            padding: EdgeInsets.only(top:10,bottom:10),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-                hintText: 'Enter your secure password'
+              Padding(
+                padding: EdgeInsets.only(top: 20, bottom: 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                      hintText: 'Enter valid mail id as abc@gmail.com'),
                 ),
               ),
-            ),
-
-          TextButton(
-              onPressed: (){ },
-              child: Text(
-                       'Forgot Password',
-              style: TextStyle(color: bgColor, fontSize: 15),
-            ),
-          ),
-          SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Connect()));
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(bgColor)),
-                child: Text('SIGN IN',
-                      style: TextStyle(
-                        fontSize: 18,
+              Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter your secure password'),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Forgot Password',
+                  style: TextStyle(color: bgColor, fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => Connect(),
                       ),
+                    );
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(bgColor),
+                  ),
+                  child: Text(
+                    'SIGN IN',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
                 ),
-                ),
-          )
-            ]
-            ))));
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
