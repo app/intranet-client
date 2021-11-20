9bbaf921b03a57af9bc16846596ac084bce98fc1
Andrey Paskal <apaskal@gmail.com>
2021-11-20 19:48:49 +0300

    Code formatting

--- lib/connect.dart
+++ lib/connect.dart
@@ -10,65 +10,79 @@ class Connect extends StatefulWidget {
 }
 
 class _ConnectState extends State<Connect> {
-
   @override
   Widget build(BuildContext context) {
     final Color bgColor = Color(int.parse("0xFF007291"));
     return Scaffold(
-        body: SingleChildScrollView(child: 
-          Padding(
-            padding: EdgeInsets.all(40),
-        child: Column(
-          children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top:70,),
-            child: Text('Connect to',
-              style:TextStyle(fontSize:40)
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(bottom:30),
-            child: Text('your intranet',
-              style:TextStyle(fontSize:40)
-            ),
-          ),
-          SizedBox(
-            width: 120,
-            height: 120,
-            child: Image.asset('asset/images/stamp.png'),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top:50,bottom:10),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                prefixIcon: Padding(padding: EdgeInsets.only(left:15,top:15,bottom:15),
-                  child: Text('https://',style:TextStyle(fontSize:16))),
-                labelText: 'Intranet URL',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 70,
                 ),
-                controller: TextEditingController()..text = 'intranet.signapi.art',
+                child: Text(
+                  'Connect to',
+                  style: TextStyle(fontSize: 40),
+                ),
               ),
-            ),
-            Text('Copy and paste or type in your intranet\'s URL',
-              style:TextStyle(fontSize:14,color: Colors.grey)
-            ),
-
-          Padding(
-            padding: EdgeInsets.only(top:30,bottom:10),
-            child: 
-          SizedBox(
-              width: double.infinity,
-              height: 50,
-              child:
-              ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(bgColor), ),
-                child: Text('CONNECT',
-                  style:TextStyle(fontSize:18,)
-                ))
-          ))
-            ]
-            ))));
+              Padding(
+                padding: EdgeInsets.only(bottom: 30),
+                child: Text(
+                  'your intranet',
+                  style: TextStyle(fontSize: 40),
+                ),
+              ),
+              SizedBox(
+                width: 120,
+                height: 120,
+                child: Image.asset('asset/images/stamp.png'),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 50, bottom: 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(left: 15, top: 15, bottom: 15),
+                      child: Text(
+                        'https://',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    labelText: 'Intranet URL',
+                  ),
+                  controller: TextEditingController()
+                    ..text = 'intranet.signapi.art',
+                ),
+              ),
+              Text('Copy and paste or type in your intranet\'s URL',
+                  style: TextStyle(fontSize: 14, color: Colors.grey)),
+              Padding(
+                padding: EdgeInsets.only(top: 30, bottom: 10),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(bgColor),
+                    ),
+                    child: Text(
+                      'CONNECT',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
